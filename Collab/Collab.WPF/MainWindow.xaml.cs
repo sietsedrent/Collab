df100618f03046076d67fbb3646e13500cb62d96
Sietse Drent (student) <s1113887@student.windesheim.nl>
2025-11-07 14:11:44 +0100

    MainWindowTest

--- Collab/Collab.WPF/MainWindow.xaml.cs
+++ Collab/Collab.WPF/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Collab.WPF
         {
             InitializeComponent();
         }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
